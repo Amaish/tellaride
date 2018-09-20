cccd7bb72b6eabe403e2b70b81ab40a604e79e44
Anthony <anthonymaina.nganga@gmail.com>
2018-09-20 21:19:17 +0300

    uploading

--- dbConnector.php
+++ dbConnector.php
@@ -4,14 +4,15 @@
 $servername = "127.0.0.1";
 $username = "root";
 $password = "Tell@ride1395";
-$password = "";
+//$password = "";
 $database = "tellaride";
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $database);
     // Check connection
     if ($conn->connect_error) {
         header('Content-type: text/plain');
-        //log error to file/db $e-getMessage()
+//echo $e-getMessage();        
+//log error to file/db $e-getMessage()
         die("END An error was encountered. Please try again later");
     }
 	//echo " END Connected successfully (".$conn->host_info.")";
