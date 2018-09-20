f84b640ae1d9db3fc5345044cdf8cf19911b4a92
Anthony <anthonymaina.nganga@gmail.com>
2018-09-20 15:47:58 +0300

    fixes

--- admin/upload.php
+++ admin/upload.php
@@ -19,7 +19,7 @@
  *  @author    Ionsolve Limited.
  *  @copyright 2017 Ionsolve Limited.
  *  @license   http://www.apache.org/licenses/LICENSE-2.0
-*/
+ */
 
 include_once($_SERVER["DOCUMENT_ROOT"] . '/tappsRide/core.php');
 
@@ -29,137 +29,209 @@ include_once($_SERVER["DOCUMENT_ROOT"] . '/tappsRide/core.php');
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title><?php echo $adminName; ?> - Contacts</title>
-	<meta content="Tappsride" name="description">
-	<meta content="width=device-width,initial-scale=1,maximum-scale=1,minimal-ui" name="viewport">
-	<meta content="IE=edge" http-equiv="X-UA-Compatible">
-	<meta content="yes" name="">
-	<meta content="black-translucent" name="">
-	<meta content="Tappsride" name="">
-	<meta content="yes" name="">
-	<link href="css/animate.css/animate.min.css" rel="stylesheet" type="text/css">
-	<link href="css/glyphicons/glyphicons.css" rel="stylesheet" type="text/css">
-	<link href="css/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-	<link href="css/material-design-icons/material-design-icons.css" rel="stylesheet" type="text/css">
-	<link href="css/ionicons/css/ionicons.min.css" rel="stylesheet" type="text/css">
-	<link href="css/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
-	<link href="css/nice.css" rel="stylesheet" type="text/css">
-	<link href="css/taggle.css" rel="stylesheet" type="text/css">
-	<link href="css/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-	<link href="css/styles/app.min.css" rel="stylesheet">
-	<link href="css/styles/font.css" rel="stylesheet" type="text/css">
-	<link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-	<style type="text/css">
-		::-webkit-scrollbar { 
-		display: none; 
-	}
-	menu, ol, ul {
-		padding: 0px 0px 0px 4px;
-	}
-	</style>
-	
-	
+    <meta charset="utf-8">
+    <title><?php
+echo $adminName;
+?> - Contacts</title>
+    <meta content="Tappsride" name="description">
+    <meta content="width=device-width,initial-scale=1,maximum-scale=1,minimal-ui" name="viewport">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <meta content="yes" name="">
+    <meta content="black-translucent" name="">
+    <meta content="Tappsride" name="">
+    <meta content="yes" name="">
+    <link href="css/animate.css/animate.min.css" rel="stylesheet" type="text/css">
+    <link href="css/glyphicons/glyphicons.css" rel="stylesheet" type="text/css">
+    <link href="css/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="css/material-design-icons/material-design-icons.css" rel="stylesheet" type="text/css">
+    <link href="css/ionicons/css/ionicons.min.css" rel="stylesheet" type="text/css">
+    <link href="css/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+    <link href="css/nice.css" rel="stylesheet" type="text/css">
+    <link href="css/taggle.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="css/styles/app.min.css" rel="stylesheet">
+    <link href="css/styles/font.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <style type="text/css">
+        ::-webkit-scrollbar { 
+        display: none; 
+    }
+    menu, ol, ul {
+        padding: 0px 0px 0px 4px;
+    }
+    </style>
+    
+    
 </head>
 <body>
 
 <div class="app" id="app">
-		<div class="app-content box-shadow-z2 pjax-container" id="content" role="main">
-			<div class="app-body">
-				<div class="app-body-inner">
-					<div class="row-col">
-						<?php include_once 'top.php'; ?>
-						<div class="row-row">
-							<div class="row-col">
-                				<?php include_once 'side.php'; ?>
-                                <div class="col-xs col-md-9" id="detail">
-									<p class="text-md text-primary" style="margin-left: 5px;">
-										Drivers Upload Report
-										<a class="btn btn-sm rounded white text-primary" style="width:170px;" href="admin.php">
-											<i class="ion-arrow-left-c"></i> Back To Dashboard
-										</a>
-										
+        <div class="app-content box-shadow-z2 pjax-container" id="content" role="main">
+            <div class="app-body">
+                <div class="app-body-inner">
+                    <div class="row-col">
+                        <?php
+include_once 'top.php';
+?>
+                       <div class="row-row">
+                            <div class="row-col">
+                                <?php
+include_once 'side.php';
+?>
+                               <div class="col-xs col-md-9" id="detail">
+                                    <p class="text-md text-primary" style="margin-left: 5px;">
+                                        Drivers Upload Report
+                                        <a class="btn btn-sm rounded white text-primary" style="width:170px;" href="admin.php">
+                                            <i class="ion-arrow-left-c"></i> Back To Dashboard
+                                        </a>
+                                        
                                     </p>
 <div class="list-group m-b">
 <?php
 
-if(isset($_POST["submit"]))
-{
+if (isset($_POST["submit"])) {
     $filename = $_FILES["file"]["tmp_name"];
-
-     if($_FILES["file"]["size"] > 0)
-     {
-         $file = fopen($filename, "r");
-         while (($emapData = fgetcsv($file, 100000, ",")) !== FALSE)
-         {
-            if (!empty($emapData[0]) AND !empty($emapData[1]) AND !empty($emapData[2]) AND !empty($emapData[3]))
-			{
-                if(strpos($emapData[1], '+') !== false) 
-                {
-
-                    $arguments = array('phonenumber' => $emapData[1]);
-                    if(returnExists('drivers', $arguments) == 0)
-                    {
-                        $name = trim($emapData[0]);
-                        $phonenumbers = trim(str_replace(' ', '', $emapData[1]));
-                        $regno = trim($emapData[2]);
-                        $engineSize = trim($emapData[3]);
-                        $sql = "INSERT INTO `drivers` (`name`, `phonenumber`,`regno`,`engineSize`) 
+    
+    if ($_FILES["file"]["size"] > 0) {
+        $file = fopen($filename, "r");
+        while (($emapData = fgetcsv($file, 100000, ",")) !== FALSE) {
+            if (!empty($emapData[0]) AND !empty($emapData[1]) AND !empty($emapData[2]) AND !empty($emapData[3])) {
+                if (strpos($emapData[1], '0') == 0) {
+                    $phone     = "+254" . substr($emapData[1], -9);
+                    $arguments = array(
+                        'phonenumber' => $phone
+                    );
+                    if (returnExists('drivers', $arguments) == 0) {
+                        $name         = trim($emapData[0]);
+                        $phonenumbers = $phone;
+                        $regno        = trim($emapData[2]);
+                        $engineSize   = trim($emapData[3]);
+                        $sql          = "INSERT INTO `drivers` (`name`, `phonenumber`,`regno`,`engineSize`) 
                         VALUES('$name','$phonenumbers','$regno','$engineSize')";
-
+                        
                         $result = mysqli_query($conn, $sql);
                         
-                        if($result)
-                        {
+                        if ($result) {
                             echo '<a class="list-group-item text-success" href="#">
                                 <span class="pull-right text-success">
                                     <i class="ion-checkmark-circled text-xs"></i>
-                                </span> Driver <b>'.$emapData[0].', '.$emapData[1].'</b> added successfully
-                            </a>'; 
-                        }else{
+                                </span> Driver <b>' . $emapData[0] . ', ' . $emapData[1] . '</b> added successfully
+                            </a>';
+                        } else {
                             echo '<a class="list-group-item text-danger" href="#">
                                 <span class="pull-right text-danger">
                                     <i class="ion-close-circled text-xs"></i>
-                                </span> Something went wrong adding driver <b>'.$emapData[0].', '.$emapData[1].'</b> 
-                            </a>'; 
+                                </span> Something went wrong adding driver <b>' . $emapData[0] . ', ' . $emapData[1] . '</b> 
+                            </a>';
                         }
-
-                    }else{
+                    } else {
                         echo '<a class="list-group-item text-danger" href="#">
                                 <span class="pull-right text-danger">
                                     <i class="ion-close-circled text-xs"></i>
-                                </span> The driver <b>'.$emapData[0].', '.$emapData[1].'</b> already exists
+                                </span> The driver <b>' . $emapData[0] . ', ' . $emapData[1] . '</b> already exists
                         </a>';
                     }
-
-                }else{
+                    
+                } else if (strpos($emapData[1], '7') == 0) {
+                    $phone     = "+254" . $emapData[1];
+                    $arguments = array(
+                        'phonenumber' => $phone
+                    );
+                    if (returnExists('drivers', $arguments) == 0) {
+                        $name         = trim($emapData[0]);
+                        $phonenumbers = $phone;
+                        $regno        = trim($emapData[2]);
+                        $engineSize   = trim($emapData[3]);
+                        $sql          = "INSERT INTO `drivers` (`name`, `phonenumber`,`regno`,`engineSize`) 
+                        VALUES('$name','$phonenumbers','$regno','$engineSize')";
+                        
+                        $result = mysqli_query($conn, $sql);
+                        
+                        if ($result) {
+                            echo '<a class="list-group-item text-success" href="#">
+                                <span class="pull-right text-success">
+                                    <i class="ion-checkmark-circled text-xs"></i>
+                                </span> Driver <b>' . $emapData[0] . ', ' . $emapData[1] . '</b> added successfully
+                            </a>';
+                        } else {
+                            echo '<a class="list-group-item text-danger" href="#">
+                                <span class="pull-right text-danger">
+                                    <i class="ion-close-circled text-xs"></i>
+                                </span> Something went wrong adding driver <b>' . $emapData[0] . ', ' . $emapData[1] . '</b> 
+                            </a>';
+                        }
+                    } else {
+                        echo '<a class="list-group-item text-danger" href="#">
+                                    <span class="pull-right text-danger">
+                                        <i class="ion-close-circled text-xs"></i>
+                                    </span> The driver <b>' . $emapData[0] . ', ' . $emapData[1] . '</b> already exists
+                            </a>';
+                    }
+                } else if (strpos($emapData[1], '+') == 0) {
+                    
+                    $arguments = array(
+                        'phonenumber' => $emapData[1]
+                    );
+                    if (returnExists('drivers', $arguments) == 0) {
+                        $name         = trim($emapData[0]);
+                        $phonenumbers = trim(str_replace(' ', '', $emapData[1]));
+                        $regno        = trim($emapData[2]);
+                        $engineSize   = trim($emapData[3]);
+                        $sql          = "INSERT INTO `drivers` (`name`, `phonenumber`,`regno`,`engineSize`) 
+                        VALUES('$name','$phonenumbers','$regno','$engineSize')";
+                        
+                        $result = mysqli_query($conn, $sql);
+                        
+                        if ($result) {
+                            echo '<a class="list-group-item text-success" href="#">
+                                <span class="pull-right text-success">
+                                    <i class="ion-checkmark-circled text-xs"></i>
+                                </span> Driver <b>' . $emapData[0] . ', ' . $emapData[1] . '</b> added successfully
+                            </a>';
+                        } else {
+                            echo '<a class="list-group-item text-danger" href="#">
+                                <span class="pull-right text-danger">
+                                    <i class="ion-close-circled text-xs"></i>
+                                </span> Something went wrong adding driver <b>' . $emapData[0] . ', ' . $emapData[1] . '</b> 
+                            </a>';
+                        }
+                        
+                    } else {
+                        echo '<a class="list-group-item text-danger" href="#">
+                                <span class="pull-right text-danger">
+                                    <i class="ion-close-circled text-xs"></i>
+                                </span> The driver <b>' . $emapData[0] . ', ' . $emapData[1] . '</b> already exists
+                        </a>';
+                    }
+                    
+                } else {
                     echo '<a class="list-group-item text-danger" href="#">
                                         <span class="pull-right text-danger">
                                             <i class="ion-close-circled text-xs"></i>
-                                        </span> The phone number <b>'.$emapData[0].', '.$emapData[1].'</b> should be in international format
+                                        </span> The phone number <b>' . $emapData[0] . ', ' . $emapData[1] . '</b> Is invalid
                     </a>';
                 }
-            }else{
+            } else {
                 echo '<a class="list-group-item text-danger" href="#">
                                         <span class="pull-right text-danger">
                                             <i class="ion-close-circled text-xs"></i>
                                         </span>Please provide a name, phone number, car registration number and engine size
                 </a>';
-            }    
-         }
-         fclose($file);
-         
-         //close of connection
-        mysqli_close($conn); 
-    }else{
-         echo '<a class="list-group-item text-danger" href="#">
+            }
+        }
+        fclose($file);
+        
+        //close of connection
+        mysqli_close($conn);
+    } else {
+        echo '<a class="list-group-item text-danger" href="#">
             <span class="pull-right text-danger">
                 <i class="ion-close-circled text-xs"></i>
             </span> Please upload a CSV file
         </a>';
-     }
-}	
+    }
+}
 
 ?>
 
